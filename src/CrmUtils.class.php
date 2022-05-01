f71c0a2d8fb8362b5bb5012691fa35a1764db0e6
ohad <ohad@theora.com>
2022-05-01 09:06:38 +0300

    gitDiffs

--- src/CrmUtils.class.php
+++ src/CrmUtils.class.php
@@ -2,15 +2,11 @@
 /*------------------------------------------------------------*/
 class CrmUtils extends Mcontroller {
 	/*------------------------------------------------------------*/
-	public static function tagName($tagId) {
-		global $Mmodel;
-		if (  ! $Mmodel )
-			$Mmodel = new Mmodel;
-		if ( ! $tagId )
-			return("(?)");
-		$sql = "select name from crmTags where id = $tagId";
-		$name = $Mmodel->getString($sql, 0);
-		return($name);
+	private $logger;
+	/*------------------------------------------------------------*/
+	public function __construct($logFile = null) {
+		parent::__construct();
+		$this->logger = new Logger($logFile);
 	}
 	/*------------------------------------------------------------*/
 	public function countries() {
@@ -24,6 +20,20 @@ class CrmUtils extends Mcontroller {
 		$countryName = $this->Mmodel->getString($sql, 24*3600);
 		return(@$countries[$countryCode]);
 	}
+	/*------------------------------------------------------------*/
+	/*------------------------------------------------------------*/
+	/*------------------------------------------------------------*/
+	public static function tagName($tagId) {
+		global $Mmodel;
+		if (  ! $Mmodel )
+			$Mmodel = new Mmodel;
+		if ( ! $tagId )
+			return("(?)");
+		$sql = "select name from crmTags where id = $tagId";
+		$name = $Mmodel->getString($sql, 24*3600);
+		return($name);
+	}
+	/*------------------------------------------------------------*/
 	/*------------------------------------------------------------*/
 }
 /*------------------------------------------------------------*/
