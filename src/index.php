f2217ac946e42d30b8f3647ccf27b9c05a192520
ohad <ohad@theora.com>
2022-05-09 11:11:47 +0300

    gitDiffs

--- src/index.php
+++ src/index.php
@@ -5,6 +5,8 @@ require_once(M_DIR."/mfiles.php");
 require_once("crmFiles.php");
 require_once("Crm.class.php");
 /*------------------------------------------------------------*/
+$startTime = microtime(true);
+/*------------------------------------------------------------*/
 $ua = @$_SERVER['HTTP_USER_AGENT'];
 if (
 	! $ua
@@ -28,7 +30,7 @@ if ( isset($_REQUEST['logOut']) ) {
 } else {
 	$crmLogin->enterSession();
 }
-$crm = new Crm;
+$crm = new Crm($startTime);
 $crm->control();
 $Mview->flushOutput();
 /*------------------------------------------------------------*/
